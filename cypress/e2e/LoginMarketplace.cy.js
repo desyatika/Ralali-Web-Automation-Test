532742c47e140b0d88189a6f0ee0c5c5879eb6d2
desyatika <desyatika95@gmail.com>
2023-03-04 18:40:31 +0700

    initial commit

---
+++ cypress/e2e/LoginMarketplace.cy.js
@@ -0,0 +1,12 @@
+describe('Login Marketplace Ralali', () => {
+    it('Login Marketplace with valid credentials', () => {
+        cy.visit('https://www.ralali.com/')
+        cy.get('.Statistics__WrapperButton-rnjxqw-6 > .Button__StyledButton-sc-1l9x0nw-0').scrollIntoView() 
+        cy.get('.Statistics__WrapperButton-rnjxqw-6 > .Button__StyledButton-sc-1l9x0nw-0').click()
+        cy.get('.RegisterForm__HeaderSection-sc-1ofjgrt-1 > .Typography__RegularH6-j4gzfj-11 > .RegisterForm__StyledLinkText-sc-1ofjgrt-8').click()
+        cy.get('[data-testid="input-username"]').type('testdesy11@gmail.com')
+        cy.get('[data-testid="input-password"]').type('Desy@123')
+        cy.get('[data-testid="button-submit"]').click()
+        cy.contains('Test Desy')
+    })
+  })
