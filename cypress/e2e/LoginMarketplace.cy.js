ca1f6ca0bd55ba1612138b75295d558fe8372387
desyatika <desyatika95@gmail.com>
2023-03-23 14:24:20 +0700

    FixSignupSignin

--- cypress/e2e/LoginMarketplace.cy.js
+++ cypress/e2e/LoginMarketplace.cy.js
@@ -1,6 +1,6 @@
 describe('Login Marketplace Ralali', () => {
     it('Login Marketplace with valid credentials', () => {
-        cy.visit('https://www.ralali.com/')
+        cy.visit('/')
         cy.get('.Statistics__WrapperButton-rnjxqw-6 > .Button__StyledButton-sc-1l9x0nw-0').scrollIntoView() 
         cy.get('.Statistics__WrapperButton-rnjxqw-6 > .Button__StyledButton-sc-1l9x0nw-0').click()
         cy.get('.RegisterForm__HeaderSection-sc-1ofjgrt-1 > .Typography__RegularH6-j4gzfj-11 > .RegisterForm__StyledLinkText-sc-1ofjgrt-8').click()
